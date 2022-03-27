c633eab7a2142d485036761228c158b7a4df550b
Duck Horse <hongsheng322@gmail.com>
2022-03-28 01:21:45 +0800

    Initial Commit

---
+++ Q2.py
@@ -0,0 +1,4 @@
+x = input("Enter X: ")
+y = input("Enter Y: ")
+average = (int(x) + int(y))/2
+print("The average of the two numbers are: " + str(average))
---
+++ Q3.py
@@ -0,0 +1,11 @@
+switch = {
+    "CSC1006": "Math 2",
+    "CSC1007": "Operating Systems",
+    "CSC1008": "Data Structures and Algorithms",
+    "CSC1009": "OOP",
+    "CSC1010": "Computer Networks"
+}
+module = "CSC1009"
+print(switch.get(module))
+for i in range(101, 65, -2):
+    print(i)
